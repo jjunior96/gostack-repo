9ebf5045b2be642a270bf5ce099291a2d1986131
Junior Alves <jjalves96@gmail.com>
2020-11-03 09:38:56 -0300

    feat: create global styles

--- 05-projeto-web/src/routes/pages/Dashboard/index.tsx
+++ 05-projeto-web/src/routes/pages/Dashboard/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import * as S from './styled';
+
 const Dashboard:React.FC = () => {
-  return <h1>Dashboard</h1>
+  return <S.Title>Dashboard</S.Title>
 }
 
 export default Dashboard;
